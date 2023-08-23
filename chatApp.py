ecba282399b3b909aa6add23a6ddf25b0f8a6094
Miriam Goldin <miriam.goldin@grunitech.com>
2023-08-23 13:54:25 +0300

    working on xhatApp.py

--- chatApp.py
+++ chatApp.py
@@ -1,34 +1,38 @@
-# from flask import Flask
-# app = Flask(__name__)
+from flask import Flask, render_template, request
+import csv
+# import win32api
 
-# @app.route("/")
-# def home():
-#     return "Hello, Flask!"
-
-# @app.route("/hello/<name>")
-# def hello_there(name):
-#     return "hello "+ name
-
-# if __name__ == "__main__":
-#   app.run(host="0.0.0.0")
-
-
-
-
-
-from flask import Flask, render_template
+USERS = "users.csv"
 
 app = Flask(__name__)
 
-# # Pass the required route to the decorator.
-# @app.route("/hello")
-# def hello():
-# 	#return render_template('index.html')
-#      return "Hello, Welcome to GeeksForGeeks"
-	
-@app.route("/")
-def index():
-	return "Homepage of GeeksForGeeks"
+def checkUserData(username, password):
+    flag = False
+    with open(USERS) as usersFile:
+        users = csv.reader(usersFile,delimiter="\n")
+        for user in users:
+            name ,passwd = user[0].split(",")
+            if name == username:
+                flag = True
+                if passwd == password:
+                    return render_template("login.html")
+                else:
+                    return "something went wrong!! check your password..."
+                    # win32api.MessageBox(0, 'check your password...', 'something went wrong!!')
+        if not flag:
+            file = open(USERS, 'a')
+            file.write(username + "," + password + "\n")
+            file.close()
+            return render_template("login.html")
 
+@app.route("/register", methods=['GET', 'POST'])
+def homePage():
+    if request.method == 'GET':
+        return render_template("register.html")
+    elif request.method == 'POST':
+        username = request.form['username']
+        userpass = request.form['password']
+        checkUserData(username, userpass)
+        
 if __name__ == "__main__":
-	app.run(host="0.0.0.0" ,debug=True)
+    app.run(host="0.0.0.0", debug=True)
